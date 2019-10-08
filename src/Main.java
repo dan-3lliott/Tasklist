fcb3787771080501d328c48ac0a2f8b55f47de2b
Daniel Elliott <elliott.daniel.ballantyne@gmail.com>
2019-10-08 11:45:51 -0600

    created seperate class for taskdialog

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 
 public class Main extends JFrame {
     private final String[] topPanelText = {"TO DO", "DO TODAY", "ONGOING"};
-    private final Color[] priorityLevels = {Color.green, Color.yellow, Color.red};
+    public final Color[] priorityLevels = {Color.green, Color.yellow, Color.red};
     public Main() {
         initComponents();
     }
@@ -13,17 +13,8 @@ public class Main extends JFrame {
         //add action listeners for components
         addTask.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent ev) {
-                taskDialog.setVisible(true);
-            }
-        });
-        confirmTaskButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent ev) {
-                tasks.add(new Task("test task", "test description", statusBox.getSelectedIndex(), priorityLevels[priorityBox.getSelectedIndex()]));
-                statusBox.setSelectedIndex(0);
-                priorityBox.setSelectedIndex(0);
-                taskDialog.dispose();
-                System.out.println(tasks.size());
-                updateTasks();
+                TaskDialog t = new TaskDialog();
+                t.setVisible(true);
             }
         });
         //set up main jframe
@@ -34,15 +25,6 @@ public class Main extends JFrame {
         setResizable(false);
         setLocationRelativeTo(null);
         //set up taskdialog jframe
-        taskDialog.setPreferredSize(new Dimension(200, 200));
-        taskDialog.setMinimumSize(new Dimension(200, 200));
-        taskDialog.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        taskDialog.setResizable(false);
-        taskDialog.setLocationRelativeTo(null);
-        mainTaskPanel.add(statusBox);
-        mainTaskPanel.add(priorityBox);
-        mainTaskPanel.add(confirmTaskButton);
-        taskDialog.add(mainTaskPanel);
         //set parameters of elements
         //set parameters of panels and add to main panel
         topMenu.setPreferredSize(new Dimension(600, 20));
@@ -54,7 +36,7 @@ public class Main extends JFrame {
         //add everything together, don't set mainPanel layout as it is flowlayout by default
         add(mainPanel);
     }
-    public void updateTasks() {
+    public static void updateTasks() {
         for (int i = 0; i < taskPanels.size(); i++) {
             for (Task t : tasks) {
                 if (t.getStatus() == i) {
@@ -80,7 +62,7 @@ public class Main extends JFrame {
             mainPanel.add(taskPanels.get(i));
         }
     }
-    public JLabel alignedJLabel(String text) {
+    public static JLabel alignedJLabel(String text) {
         JLabel alignedLabel = new JLabel(text);
         alignedLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
         return alignedLabel;
@@ -89,23 +71,17 @@ public class Main extends JFrame {
         EventQueue.invokeLater(new Runnable() {
             @Override
             public void run() {
-                Main testProject = new Main();
-                testProject.setVisible(true);
+                Main taskList = new Main();
+                taskList.setVisible(true);
             }
         });
     }
     //variable declarations
-    private FlowLayout mainLayout = new FlowLayout(FlowLayout.CENTER, 5, 5);
-    private JFrame taskDialog = new JFrame("Add Task");
-    private JPanel mainTaskPanel = new JPanel();
-    private JComboBox priorityBox = new JComboBox(new String[]{"Not Urgent", "Moderate", "Urgent"});
-    private JComboBox statusBox = new JComboBox(new String[]{"To Do", "Do Today", "Ongoing"});
-    private JButton confirmTaskButton = new JButton("Confirm");
-    private JPanel mainPanel = new JPanel(mainLayout);
-    private ArrayList<JPanel> taskPanels = new ArrayList<JPanel>();
-    private ArrayList<JPanel> topPanels = new ArrayList<JPanel>();
+    private static JPanel mainPanel = new JPanel(); //flowlayout with padding of 5 px by default
+    private static ArrayList<JPanel> taskPanels = new ArrayList<JPanel>();
+    private static ArrayList<JPanel> topPanels = new ArrayList<JPanel>();
+    public static ArrayList<Task> tasks = new ArrayList<Task>();
     private JMenuBar topMenu = new JMenuBar();
     private JMenu addMenu = new JMenu("Add");
     private JMenuItem addTask = new JMenuItem("Add Task");
-    private ArrayList<Task> tasks = new ArrayList<Task>();
 }
